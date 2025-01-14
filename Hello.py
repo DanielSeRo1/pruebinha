b52b41d6eb30fae2bc8cde3bd5688b0782c90f4d
DanielSeRo1 <al2233802556@azc.uam.mx>
2025-01-14 12:33:25 -0600

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -6,7 +6,9 @@ st.set_page_config()
 enable = st.checkbox("Activar cámara")
 ph = st.empty()
 N = 15
+ph.metric("Cuenta regresiva", f"{mm:02d}:{ss:02d}")
 picture = st.camera_input("Tomar foto", disabled = not enable)
+  
 
 
 
