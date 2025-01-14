baa756f9d927431673226db83a7b2ccc0699f5b2
DanielSeRo1 <al2233802556@azc.uam.mx>
2025-01-14 12:15:56 -0600

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -6,10 +6,13 @@ st.set_page_config()
 enable = st.checkbox("Activar cámara")
 ph = st.empty()
 N = 1*60
+
+
 for secs in range(N,0,-1):
 	mm, ss = secs//60, secs%60
 	ph.metric("Cuenta regresiva", f"{mm:02d}:{ss:02d}")
 	time.sleep(1)
+if secs == 0
 	st.camera_input
 st.image(picture)
 
