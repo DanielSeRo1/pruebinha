42f2d1ea8e98990f5663be0b484dbef59910e431
DanielSeRo1 <al2233802556@azc.uam.mx>
2025-01-14 11:49:16 -0600

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -5,7 +5,7 @@ st.set_page_config()
 
 ph = st.empty()
 N = 5*60
-for secs i range(N,0,-1):
+for secs in range(N,0,-1):
 	mm, ss = secs//60, secs%60
 	ph.metric("Cuenta regresiva", f"{mm:02d}:{ss:02d}")
 	time.sleep(1)
