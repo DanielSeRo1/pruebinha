0eb8ec30469e1642c4b1b57bfc53caa535b93822
DanielSeRo1 <al2233802556@azc.uam.mx>
2025-01-14 11:53:55 -0600

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -12,5 +12,6 @@ if picture:
 	for secs in range(N,0,-1):
 		mm, ss = secs//60, secs%60
 		ph.metric("Cuenta regresiva", f"{mm:02d}:{ss:02d}")
-		st.image(picture)
 		time.sleep(1)
+	st.image(picture)
+
