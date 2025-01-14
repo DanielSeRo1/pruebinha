c1df002c6376aa5687cf52e0e6d920f680936e91
DanielSeRo1 <al2233802556@azc.uam.mx>
2025-01-14 12:17:55 -0600

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -7,12 +7,12 @@ enable = st.checkbox("Activar cámara")
 ph = st.empty()
 N = 1*60
 
-
-for secs in range(N,0,-1):
-	mm, ss = secs//60, secs%60
-	ph.metric("Cuenta regresiva", f"{mm:02d}:{ss:02d}")
-	time.sleep(1)
-st.camera_input
+picture = st.camera_input("Tomar foto", disabled = not enable)
+if picture:
+	for secs in range(N,0,-1):
+		mm, ss = secs//60, secs%60
+		ph.metric("Cuenta regresiva", f"{mm:02d}:{ss:02d}")
+		time.sleep(1)
 st.image(picture)
 
 
