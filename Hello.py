70b1ee8185be86ffa9db61200292944190b45d0d
DanielSeRo1 <al2233802556@azc.uam.mx>
2025-01-14 11:46:05 -0600

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -1,7 +1,14 @@
 import streamlit as st
+import time
 
 enable = st.checkbox("Activar cámara")
 picture = st.camera_input("Tomar foto", disabled = not enable)
 
 if picture:
+	ph = st.empty()
+	N = 5*60
+	for secs i range(N,0,-1):
+		mm, ss = secs//60, secs%60
+		ph.metric("Cuenta regresiva", f"{mm:02d}:{ss:02d}")
+		time.sleep(1)
 	st.image(picture)
